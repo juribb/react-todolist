016899dcf1ca64ba7a0bf2c6c73d893c1440eb65
juriChoi <jrii22e@gmail.com>
2021-04-11 22:36:20 +0900

    react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {createGlobalStyle} from 'styled-components';
+
+
+function App() {
+  return (
+   <>
+    hello world    
+   </>
+  );
+}
+
+export default App;
