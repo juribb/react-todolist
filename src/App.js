6f398990a91489ddd94bf07732e5ae1cd5ff4d39
juriChoi <jrii22e@gmail.com>
2021-04-18 20:22:47 +0900

    add components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import {createGlobalStyle} from 'styled-components';
-//import TodoTemplate from './components/TodoTemplate';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TodoHead';
 
 const GlobalStyle = createGlobalStyle`
-  body{
-    background : #e9ecef;
+  body {
+    background: #e9ecef;
   }
 `;
 
 function App() {
   return (
-   <>
-    <GlobalStyle/>
-    <TodoTemplate>hello world</TodoTemplate>    
-   </>
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+      </TodoTemplate>
+    </>
   );
 }
 
